1bb5e906e08f85e755d94c68b3024b1aa4168182
claramendrod <claramendrod@gmail.com>
2026-05-28 21:56:40 -0300

    Add files via upload
    Atividade Final: Gestão de estoque e auditoria de dados - Clara

---
+++ catalogo_clara/src/main/java/br/com/fatec/catalogo/security/SecurityConfig.java
@@ -0,0 +1,42 @@
+package br.com.fatec.catalogo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/categorias/**").hasRole("ADMIN")
+                        .requestMatchers("/usuarios/**").hasRole("ADMIN") // Somente Admin cadastra novos usuários
+                        .requestMatchers("/produtos").permitAll() // Público
+                        .requestMatchers("/produtos/novo", "/produtos/salvar", "/produtos/editar/**",
+                                "/produtos/excluir/**", "/produtos/historico").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login") // Define a rota da página customizada
+                        .defaultSuccessUrl("/produtos", true)
+                        .permitAll()
+                )
+                .logout(logout -> logout.logoutSuccessUrl("/produtos"));
+                
+
+        return http.build();
+    }
+
+}
